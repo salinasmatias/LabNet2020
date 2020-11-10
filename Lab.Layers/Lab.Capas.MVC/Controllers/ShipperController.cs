83fa5e848900f70c9e9f56f22ff756df018a5afd
Matias <salinasmatias1990@gmail.com>
2020-11-10 19:00:20 -0300

    Added model, view and controller for shippers

---
+++ Lab.Layers/Lab.Capas.MVC/Controllers/ShipperController.cs
@@ -0,0 +1,28 @@
+﻿using Lab.Capas.MVC.Models;
+using Lab.Demo.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.Capas.MVC.Controllers
+{
+    public class ShipperController : Controller
+    {
+        // GET: Shipper
+        public ActionResult Index()
+        {
+            var logic = new ShippersLogic();
+            var shippers = logic.GetAll();
+            List<ShipperView> shipperViews = (from shipper in shippers
+                                              select new ShipperView()
+                                              {
+                                                  ShipperID = shipper.ShipperID,
+                                                  CompanyName = shipper.CompanyName,
+                                                  Phone = shipper.Phone
+                                              }).ToList();
+            return View(shipperViews);
+        }
+    }
+}
---
+++ Lab.Layers/Lab.Capas.MVC/Models/ShipperView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab.Capas.MVC.Models
+{
+    public class ShipperView
+    {
+        public int ShipperID { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
