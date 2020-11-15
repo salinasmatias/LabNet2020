5bc6ea530ad525687952f6474df63ec26b8d695f
Matias <salinasmatias1990@gmail.com>
2020-11-15 13:39:57 -0300

    Adding quiz api

---
+++ Lab.Layers/Lab.Capas.MVC/Controllers/QuizController.cs
@@ -0,0 +1,52 @@
+﻿using Lab.Capas.MVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.Capas.MVC.Controllers
+{
+    public class QuizController : Controller
+    {
+        // GET: Quiz
+        public ActionResult Index()
+        {
+            string response = getHttp();
+            QuizApiView quiz = JsonConvert.DeserializeObject<QuizApiView>(response);
+            return View(quiz.results);
+        }
+
+        public ActionResult GetScore(IList<Result> results)
+        {
+            int correctAnswers = 0;
+            foreach(var question in results)
+            {
+                if(question.correct_answer == question.userResponse)
+                {
+                    correctAnswers++;
+                    question.userResponse = "Correct";
+                }
+                else
+                {
+                    question.userResponse = "wrong";
+                }
+            }
+            TempData["UserScore"] = correctAnswers;
+            TempData["Results"] = results;
+
+            return View("Index");
+        }
+
+        public static string getHttp()
+        {
+            WebRequest oRequest = WebRequest.Create("https://opentdb.com/api.php?amount=10&category=15&type=boolean");
+            WebResponse oResponse = oRequest.GetResponse();
+            StreamReader sr = new StreamReader(oResponse.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+    }
+}
---
+++ Lab.Layers/Lab.Capas.MVC/Models/QuizApiView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.Capas.MVC.Models
+{
+    public class QuizApiView
+    {
+        public int response_code { get; set; }
+        public IList<Result> results { get; set; }
+    }
+    public class Result
+    {
+        public string category { get; set; }
+        public string type { get; set; }
+        public string difficulty { get; set; }
+        [AllowHtml]
+        public string question { get; set; }
+        public string correct_answer { get; set; }
+        public IList<string> incorrect_answers { get; set; }
+        public string userResponse { get; set; } = "test";
+    }
+}
