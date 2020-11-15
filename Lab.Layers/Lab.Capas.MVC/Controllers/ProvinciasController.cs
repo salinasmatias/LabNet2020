6c12bfaded97e44db68dd1809879322396ffaade
Matias <salinasmatias1990@gmail.com>
2020-11-15 03:42:09 -0300

    Made a GET request to API datos.gob.ar and made the necessary view, controller and models to display the data of said request

---
+++ Lab.Layers/Lab.Capas.MVC/Controllers/ProvinciasController.cs
@@ -0,0 +1,31 @@
+﻿using Lab.Capas.MVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.Capas.MVC.Controllers
+{
+    public class ProvinciasController : Controller
+    {
+        // GET: Provincias
+        public ActionResult Index()
+        {
+            string respuesta = getHttp();
+            Example pais = JsonConvert.DeserializeObject<Example>(respuesta);
+            return View(pais);
+        }
+
+        public static string getHttp()
+        {
+            WebRequest oRequest = WebRequest.Create("https://apis.datos.gob.ar/georef/api/provincias");
+            WebResponse oResponse = oRequest.GetResponse();
+            StreamReader sr = new StreamReader(oResponse.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+    }
+}
---
+++ Lab.Layers/Lab.Capas.MVC/Models/Example.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab.Capas.MVC.Models
+{
+    public class Example
+    {
+        public int cantidad { get; set; }
+        public int inicio { get; set; }
+        public Parametros parametros { get; set; }
+        public IList<Provincia> provincias { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Parametros
+    {
+    }
+
+    public class Centroide
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+
+    public class Provincia
+    {
+        public Centroide centroide { get; set; }
+        public string id { get; set; }
+        public string nombre { get; set; }
+    }
+}
